56e461b336605a22cb484273fb10dee2fd4994e6
Andy Ta <Andy Ta@SURFACE-ANDY.wireless.concordia.ca>
2017-01-21 16:22:35 -0500

    Button events for budget list page

--- www/js/views/MainView.js
+++ www/js/views/MainView.js
@@ -6,8 +6,47 @@ var MainView = function(){
     };
     
     this.setListeners = function() {
-        $('#add').click(function(){
+        var dropdown = function() {
+            $('.dropdown-menu li').click(function(e){
+                e.preventDefault();
+                if($(this).children("a").text() !== "Other") {
+                    $(".dropdown-menu li").removeClass("active");
+                    $(this).addClass("active");
+                    $(this).parents().eq(2).find("span:first").remove();
+                    $(this).parents().eq(2).find("button:first").prepend("<span>" + $(this).children("a").text() + "</span>");
+                }
+                else {
+                    $(this).parents().eq(1).replaceWith("<input type='text' class='form-control' placeholder='Other'></input>");
+                }
+            });
+        }
+        
+        dropdown();
+        
+        $('#add').click(function(e){
+            e.preventDefault();
+            var form = '<form class="form-inline" style="margin-top: 2%">\n\
+                            <div class="form-group">\n\
+                                <div class="row">\n\
+                                    <div class="col-xs-6">\n\
+                                        <div class="dropdown">\n\
+                                            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="width: 100%;"><span>Category</span><span class="caret"></span></button>\n\
+                                            <ul class="dropdown-menu">\n\
+                                                <li><a href="#">Food</a></li>\n\
+                                                <li><a href="#">Clothes</a></li>\n\
+                                                <li class="divider"></li><li><a id="other" href="#">Other</a></li>\n\
+                                            </ul>\n\
+                                        </div>\n\
+                                    </div>\n\
+                                    <div class="col-xs-6">\n\
+                                        <input type="text" class="form-control" id="priceInput" placeholder="Budget limit (E.g. $100)">\n\
+                                    </div>\n\
+                                </div>\n\
+                            </div>\n\
+                        </form>'
             
+            $('.form-inline:last').after(form);
+            dropdown();
         });
     };
     
